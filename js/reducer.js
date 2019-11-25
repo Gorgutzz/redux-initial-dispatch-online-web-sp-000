ab2d7bdf418733c7bbe527fd9102be0cfc7639e0
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-11-25 00:37:00 +0000

    Automatically backed up by Learn

--- js/reducer.js
+++ js/reducer.js
@@ -1,7 +1,6 @@
-let state = {count: 0};
+let state;
 
-function changeState(state, action) {
-
+function changeState(state = { count: 0 }, action) {
     switch (action.type) {
 
       case 'INCREASE_COUNT':
@@ -21,4 +20,4 @@ function render(){
   document.body.textContent = state.count
 }
 
-dispatch({ type: '@@INIT' })
+dispatch({type: '@@INIT'})
